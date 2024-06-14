3f1e14add5066fba1f539ec32b5f0b58dfdc4b2e
Sean Maden <metamaden@users.noreply.github.com>
2024-06-13 17:24:49 -0700

    adds new js, css, styles organization
    adds new inst/styles/* files to manage page specific styles
    updates .js, .css styles for collapsible menus, new background

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 // script.js
 
 function openPage(pageName) {
+    closeNav();
     const tabContents = document.getElementsByClassName('tab-content');
     for (const content of tabContents) {
         content.style.display = 'none';
@@ -11,3 +12,11 @@ function openPage(pageName) {
 
 // Set the default tab (e.g., 'home')
 openPage('home');
+
+function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+}
